1da609dfcfb2b8a1a2b096f8c647d580b057670f
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-03-23 02:30:07 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express= require("express")
+const app=express();
+
+app.use(express.static('public'));
+
+app.all('/',(req,res)=>{
+	res.render("index.ejs");
+})
+
+
+var env = process.argv[2] || 'dev';
+switch (env) {
+    case 'dev':
+        app.listen(3000, ()=>{
+			console.log("Running");
+		});
+        break;
+    case 'prod':
+        app.listen();
+        break;
+}
