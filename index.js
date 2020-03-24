da83b14ae440721f6b5c1d51f31700cf5e60f570
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-03-23 20:38:56 -0500

    Basico

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express= require("express")
-const morgan= require("morgan")
+/*const morgan= require("morgan")
 const errorHandler = require('strong-error-handler');
 const writeErrorToResponse = require('strong-error-handler').writeErrorToResponse;
-const errHandlingOptions = {debug: true}
+const errHandlingOptions = {debug: true}*/
 const app=express();
-var env = process.argv[2] || 'dev';
-app.set('view engine', 'ejs');
-const bodyParser = require('body-parser');
+//var env = process.argv[2] || 'dev';
+//app.set('view engine', 'ejs');
+//const bodyParser = require('body-parser');
 
-//app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static('public'));
-app.use(morgan('dev'));
-app.use(express.json());
+//app.use(morgan('dev'));
+/*app.use(express.json());
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
     extended: true,
     parameterLimit: 50000
   })
-);
+);*//*
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
@@ -41,7 +40,7 @@ switch (env) {
         var host="localhost"
         break;
 }
-/*
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : host,
@@ -59,19 +58,19 @@ connection.connect(function(error){
 connection.end();
 */
 app.get('/',(req,res)=>{
-	res.render("index.ejs");
+	//res.render("index.ejs");
+  res.send("Hello")
 });
 
-app.all('/auth',(req,res)=> {
-    res.render("wait.ejs");
+app.all('/auth',function (req,res){
+    //res.render("wait.ejs");
+    res.send("World");
 });
 
-
+var env='prod';
 switch (env) {
     case 'dev':
-        app.listen(3000, ()=>{
-			console.log("Running");
-		});
+        app.listen(3000, ()=>{console.log("Running");});
     //app.listen();
         break;
     case 'prod':
