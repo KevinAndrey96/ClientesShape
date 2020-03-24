e32acd48341b53ff0ffc30f00d5dd4daafdd90a7
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-03-23 20:17:50 -0500

    CORS

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(
     parameterLimit: 50000
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(errorHandler({
   debug: true,
   log: true,
