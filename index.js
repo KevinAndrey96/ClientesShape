ff54766a666902e8cc6710c1a7dffd0e50c6ea6d
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-03-30 19:45:10 -0500

    Login funcional

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express= require("express")
 const morgan= require("morgan")
 const { User } = require('./models')
 const crypto = require("crypto")
-const swal = require('sweetalert');
+const passport = require('passport');
+//const swal = require('sweetalert');
 const session = require('express-session')
-var cookieParser = require('cookie-parser')
+//var cookieParser = require('cookie-parser')
 
-const errorHandler = require('strong-error-handler');
-const writeErrorToResponse = require('strong-error-handler').writeErrorToResponse;
-const errHandlingOptions = {debug: true}
+//const errorHandler = require('strong-error-handler');
+//const writeErrorToResponse = require('strong-error-handler').writeErrorToResponse;
+//const errHandlingOptions = {debug: true}
 const app=express();
 var env = process.argv[2] || 'dev';
 app.set('view engine', 'ejs');
@@ -23,7 +24,7 @@ app.use(
     extended: true,
     parameterLimit: 50000
   })
-);
+  );
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
@@ -32,36 +33,87 @@ app.use((req, res, next) => {
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-app.use(errorHandler({
+/*app.use(errorHandler({
   debug: true,
   log: true,
-}));
+}));*/
 app.use(bodyParser.json({ limit: "50mb" }));
 
-switch (env) {
-    case 'dev':
-        var host="67.205.184.22"
-        break;
-    case 'prod':
-        var host="localhost"
-        break;
-}
-
-app.use(cookieParser());
+/*app.use(cookieParser());*/
+app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: true }
+  secret: 'keyboardcat',
+  cookie: {
+    path    : '/',
+    httpOnly: false,
+    maxAge  : 24*60*60*1000
+  }
 }))
 
 
-app.get('/',(req,res,next)=>{
-	res.render("index",{login: "None"});
-  //res.send("Hello");
-  next();
+app.use(passport.initialize());
+app.use(passport.session());
+//require('./path/to/passport/config/file')(passport);
+
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
+const NisAuthenticated = function (req, res, next) {
+  if (!req.isAuthenticated())
+    return next();
+  res.redirect('/dashboard');
+}
+passport.use(new LocalStrategy(
+  { 
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+  function(req, email, password, done) { 
+    const user = User.findOne({
+            where: {
+                email: email,
+                password: crypto.createHash('sha1').update(password).digest('hex')
+            }
+        }).then(function(user) {
+          
+            if (!user) {
+              console.log("Datos incorrectos")
+                return done(null, false, {
+                    message: 'Datos incorrectos'
+                });
+            }
+            var userinfo = user.get();
+            return done(null, userinfo);
+        }).catch(function(err) {
+            console.log("Error:", err);
+            return done(null, false, {
+                message: 'Something went wrong with your Signin'
+            });
+        });
+    }
+  ));
+
+
+
+app.get('/',NisAuthenticated,(req,res,next)=>{
+  if(req.query.error)
+  {
+    res.render("index",{login: "Wrong"});
+  }
+	else
+  {
+    res.render("index",{login: "None"});
+  }
+
 });
-app.post('/',(req,res)=>{
+/*app.post('/',(req,res)=>{
 
 const users = User.findOne({
    where: {
@@ -74,7 +126,7 @@ const users = User.findOne({
     res.render("index",{login: "Wrong"})
   }else
   {
-    session.USER=users.email
+    req.session.USER=users.email
     
     res.redirect("/dashboard");
     //res.send("SEssion correcta"+req.session.USER);
@@ -83,24 +135,64 @@ const users = User.findOne({
   console.log('Oops! something went wrong, : ', err);
 });
   
-})
-
-app.get('/dashboard',function (req,res,next){
-  if(session.USER){ 
-      res.send('Hola: ' + session.USER);
-   }else{
-      res.redirect("/")
+})*/
+app.post('/',(req,res,next)=>{
+  /*  passport.authenticate('local', { 
+    successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash : true
+  })*/
+  passport.authenticate('local', function(err, user, info) {
+    console.log(user)
+    if (err) { return next(err); }
+    if (!user) {
+     return res.redirect('/?error=true'); 
    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/dashboard');
+    });
+  })(req, res, next);
+});
+
+app.get('/dashboard', isAuthenticated ,function (req,res,next){
+  res.send("Bienvenido"+req.user.firstName)
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  /*User.findById(id, function(err, user) {
+    cb(err, user);
+  });*/
+  User.findOne({
+        where: {
+           id: id
+        }
+     }).then(function(user) {
+        if (!user) {
+            return 'ERROR 1234';
+        }
+        cb(null, user);
+     });
 });
 
 switch (env) {
-    case 'dev':
-        app.listen(3000, ()=>{
-          console.log("Running");
-        });
+  case 'dev':
+  app.listen(3000, ()=>{
+    console.log("Running");
+  });
     //app.listen();
-        break;
+    break;
     case 'prod':
-        app.listen();
-        break;
-}
+    app.listen();
+    break;
+  }
